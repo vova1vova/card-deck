c97e06ab6847c9631134519c445c34a254d7700b
Vladimir Ryabchikov <vladimir.ryabchikov@gmail.com>
2014-10-14 16:51:06 +0400

    update build gradle

--- Sample/src/main/java/com/redwayapps/carddecksample/activities/SampleActivity.java
+++ Sample/src/main/java/com/redwayapps/carddecksample/activities/SampleActivity.java
@@ -9,7 +9,7 @@ import com.redwayapps.carddecksample.fragments.MainFragment;
 import com.redwayapps.carddecksample.fragments.MenuFragment;
 import com.redwayapps.carddecksample.fragments.SubmenuFragment;
 import com.snaprix.carddecklibrary.views.Side;
-import com.snaprix.carddecklibrary.views.SlideContainer;
+import com.snaprix.carddecklibrary.views.SlideController;
 
 public class SampleActivity extends ActionBarActivity{
     private static final String FRAGMENT_MAIN = "fragment_main";
@@ -21,14 +21,14 @@ public class SampleActivity extends ActionBarActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SlideContainer slideContainer = new SlideContainer(this, Side.RIGHT);
+        SlideController slideContainer = new SlideController(this, Side.RIGHT);
 
         if (savedInstanceState == null){
             getSupportFragmentManager()
                     .beginTransaction()
                     .add(R.id.content, new MainFragment(), FRAGMENT_MAIN)
-                    .add(com.snaprix.carddecklibrary.R.id.card_2_interact, new MenuFragment(), FRAGMENT_MENU)
-                    .add(com.snaprix.carddecklibrary.R.id.card_3_interact, new SubmenuFragment(), FRAGMENT_SUB_MENU)
+                    .add(com.snaprix.carddecklibrary.R.id.card_2_layer, new MenuFragment(), FRAGMENT_MENU)
+                    .add(com.snaprix.carddecklibrary.R.id.card_3_layer, new SubmenuFragment(), FRAGMENT_SUB_MENU)
                     .commit();
         }
     }
